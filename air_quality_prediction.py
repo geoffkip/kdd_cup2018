6f0e0023583a4ea9051a71f5caa382c2a11fda85
gjerome1991 <g_kip@live.com>
2018-03-21 22:56:15 -0400

    read in data

---
+++ air_quality_prediction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 22:50:19 2018
+
+@author: geoffrey.kip
+"""
+
+import pandas as pd
+import numpy as np
+
+meo_data= pd.read_csv("beijing_17_18_meo.csv")
+aq_data= pd.read_csv("beijing_17_18_aq.csv")
