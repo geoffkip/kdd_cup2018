0136dc2405fb2ec43cfe27a1e83492dbcca17cdd
gjerome1991 <g_kip@live.com>
2018-03-25 20:50:53 -0400

    Prepare meteorological data

--- air_quality_prediction.py
+++ air_quality_prediction.py
@@ -21,7 +21,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 from sklearn.preprocessing import StandardScaler
-
+from datetime import datetime
+ 
 meo_data= pd.read_csv("beijing_17_18_meo.csv")
 aq_data= pd.read_csv("beijing_17_18_aq.csv")
 
@@ -58,3 +59,43 @@ for i, col in enumerate(meo_data[quant_columns]):
     plt.figure(i)
     sns.distplot(meo_data[col])
     
+# Drop latitude and longitude as this can be inferred by the station id
+meo_data= meo_data.drop(["latitude", "longitude"], axis=1)
+categorical_data= meo_data.select_dtypes(include=[np.object])
+
+# Do some feature engineering on dates. I am guessing time of the year plays a big role in air quality
+day_of_week = lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S" ).weekday()
+month = lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S" ).month
+week_number = lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S" ).strftime('%V')
+
+# Code seasons
+seasons = [0,0,1,1,1,2,2,2,3,3,3,0] #dec - feb is winter, then spring, summer, fall etc
+season = lambda x: seasons[(datetime.strptime(x, "%Y-%m-%d %H:%M:%S" ).month-1)]
+ 
+# sleep: 12-5, 6-9: breakfast, 10-14: lunch, 14-17: dinner prep, 17-21: dinner, 21-23: deserts!
+times_of_day = [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5 ]
+time_of_day = lambda x: times_of_day[(datetime.strptime(x, "%Y-%m-%d %H:%M:%S").hour)]
+
+
+# Create new date variables
+categorical_data['day_of_week'] = categorical_data['utc_time'].map(day_of_week)
+categorical_data['month'] = categorical_data['utc_time'].map(month)
+categorical_data['week_number'] = categorical_data['utc_time'].map(week_number)
+categorical_data['season'] = categorical_data['utc_time'].map(season)
+#categorical_data['time_of_day'] = categorical_data['utc_time'].map(time_of_day)
+categorical_data= categorical_data.drop(["utc_time"],axis=1)
+
+# Do some binary encoding for time variables
+#categorical_data["weather_cat"] = categorical_data["weather"].astype('category').cat.codes
+
+# one hot encode station_id and weather
+categorical_data= pd.get_dummies(categorical_data, columns=["station_id"])
+categorical_data= pd.get_dummies(categorical_data, columns=["weather"])
+
+# Drop categorical variables from meo_data
+meo_data= meo_data.drop(["weather","utc_time"], axis=1)
+
+meo_data_prepped= pd.merge(meo_data, categorical_data, left_index=True, right_index=True)
+
+
+
